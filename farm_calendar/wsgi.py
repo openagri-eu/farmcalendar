651d8cbd0b403379348281216415f496ffe3cdaf
Bapat <P70074418@unimaas.nl>
2024-05-16 15:16:11 +0200

    project renamed

--- farm_calendar/wsgi.py
+++ farm_calendar/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openagri_farm_calendar.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farm_calendar.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openagri_farm_calendar.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farm_calendar.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
