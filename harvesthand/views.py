fd4b63c9c2c9cb51dd148a717543ab6a352c1a12
Felipe Arruda Pontes <felipe.arrudapontes@maastrichtuniversity.nl>
2024-06-03 12:17:33 +0200

    simple view and login template refs #1 will check if the idea is correct. Or are going for a centralized UI service instead?

--- harvesthand/views.py
+++ harvesthand/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(f"{settings.LOGIN_URL}?next={request.path}")
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'auth/login.html')
