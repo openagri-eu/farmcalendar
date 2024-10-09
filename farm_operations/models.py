61572dff160d212a0d7cdbb174beb393fb9792f8
Felipe Arruda Pontes <felipe.arrudapontes@maastrichtuniversity.nl>
2024-10-09 09:50:48 +0200

    saving these just so that I dont loose track of changes

--- farm_operations/models.py
+++ farm_operations/models.py
@@ -5,6 +5,9 @@ from django.conf import settings
 
 
 class FarmOperationType(models.Model):
+    id = models.AutoField(primary_key=True, db_index=True, editable=False, unique=True,
+                          blank=False, null=False, verbose_name='ID')
+
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     # Fields for color codes
@@ -29,11 +32,19 @@ class FarmOperationType(models.Model):
 
 
 class FarmOperation(models.Model):
-    operation_type = models.ForeignKey(FarmOperationType, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True, db_index=True, editable=False, unique=True,
+                          blank=False, null=False, verbose_name='ID')
+
     title = models.CharField(max_length=200)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
     details = models.TextField(blank=True, null=True)
+
+    operation_type = models.ForeignKey(FarmOperationType, on_delete=models.CASCADE)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+
+    responsible_agent = models.CharField(blank=True, null=True)
+    agricultural_machinery = models.ManyToManyField('farm_management.AgriculturalMachine', related_name='used_in_operations', blank=True, null=True)
+    # weather_observation = models.ManyToManyField('farm_management.AgriculturalMachine', related_name='used_in_operations', blank=True, null=True)?
 
     def __str__(self):
         return f"{self.title} ({self.start_time.strftime('%Y-%m-%d %H:%M')})"
