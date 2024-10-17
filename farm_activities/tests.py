d7a7282cb7a27e9362aa65ac8c76bb8076ea978b
Felipe Arruda Pontes <felipe.arrudapontes@maastrichtuniversity.nl>
2024-10-17 18:00:16 +0200

    adding simple test for now just to start with something
    testing if the endpoint that retrieves the calendar activities returns status 200 (basically test that this endpoint exist for us to use on the web ui view)

--- farm_activities/tests.py
+++ farm_activities/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class FarmActivitiesTests(TestCase):
+
+    def setUp(self):
+        # Create a user to be logged in, as the views require login
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+
+    def test_calendar_activity_list_returns_200(self):
+        # Log in the test client as the user (since the view requires login)
+        self.client.login(username='testuser', password='testpass')
+
+        # Get the URL for the 'calendar_activity_list' view
+        url = reverse('calendar_activity_list')
+
+        # Send a GET request to the view
+        response = self.client.get(url)
+
+        # Assert that the response status code is 200 (OK)
+        self.assertEqual(response.status_code, 200)
