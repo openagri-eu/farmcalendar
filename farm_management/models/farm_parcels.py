2794c03efc4bf7ae69eced0f3ef592c631c2ae78
Bapat <P70074418@unimaas.nl>
2024-10-21 17:45:12 +0200

    unwanted models removed. requirements improve.

--- farm_management/models/farm_parcels.py
+++ farm_management/models/farm_parcels.py
@@ -33,38 +33,3 @@ class FarmParcel(NamedHistoricalBaseModel):
 
     def __str__(self):
         return f"{self.name} ({self.cultivation_type})"
-
-
-class Activity(models.Model):
-    class ActivityTypeChoices(models.TextChoices):
-        IRRIGATION = 'irrigation', _('Irrigation')
-        PEST_MANAGEMENT = 'pest_management', _('Pest Management')
-        HARVESTING = 'harvesting', _('Harvesting')
-        SCOUTING = 'scouting', _('Scouting')
-
-    parcel = models.ForeignKey(FarmParcel, on_delete=models.CASCADE, related_name="activities")
-    date = models.DateField()
-    activity_type = models.CharField(max_length=50, choices=ActivityTypeChoices.choices)
-    description = models.TextField(blank=True, null=True)  # Additional details
-
-    def __str__(self):
-        return f"{self.activity_type} on {self.date} ({self.parcel})"
-
-
-class Observation(models.Model):
-    class ObservationTypeChoices(models.TextChoices):
-        GROWTH_STAGE = 'growth_stage', _('Growth Stage')
-        PEST_INFESTATION = 'pest_infestation', _('Pest Infestation')
-        DISEASE_INFESTATION = 'disease_infestation', _('Disease Infestation')
-        FRUITS_COUNTED = 'fruits_counted', _('Fruits Counted')
-        CANOPY_DETECTED = 'canopy_detected', _('Canopy Detected')
-
-    parcel = models.ForeignKey(FarmParcel, on_delete=models.CASCADE, related_name="observations")
-    observation_date = models.DateField()
-    observation_type = models.CharField(max_length=50, choices=ObservationTypeChoices.choices)
-    value = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.observation_type} on {self.observation_date} for {self.parcel}"
-
