be0d1c0027568ab68a0d3f4c8681f9e184c96c16
Felipe Arruda Pontes <felipe.arrudapontes@maastrichtuniversity.nl>
2024-10-29 12:29:33 +0100

    fixing named historical model

--- farm_management/models/base.py
+++ farm_management/models/base.py
@@ -60,22 +60,9 @@ class ActivePageManager(models.Manager):
         return super().get_queryset().filter(status__lt=BaseModel.BaseModelStatus.DELETED)
 
 
-class NamedHistoricalBaseModel(models.Model):
-    id = models.AutoField(primary_key=True, db_index=True, editable=False, unique=True,
-                          blank=False, null=False, verbose_name='ID')
+class NamedHistoricalBaseModel(BaseModel):
 
     name = models.CharField(max_length=100)
-
-    class StatusChoices(models.IntegerChoices):
-        ACTIVE = 0, _('Active')
-        DELETED = 1, _('Deleted')
-
-    status = models.IntegerField(choices=StatusChoices, default=StatusChoices.ACTIVE)
-    deleted_at = models.DateTimeField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    history = HistoricalRecords(inherit=True)
 
     class Meta:
         abstract = True
